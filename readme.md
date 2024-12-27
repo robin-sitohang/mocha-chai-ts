c637e7fd40dd0c4486ddda34bf67d9bf615bacc2
Robin Valentino Sitohang <robinvalentinositohang@Robins-MacBook-Air.local>
2024-12-27 11:35:43 +0700

    add readme.md

---
+++ readme.md
@@ -0,0 +1,15 @@
+#### How to set up
+```
+git clone https://github.com/robin-sitohang/mocha-chai-ts.git
+
+npm install
+```
+
+#### How To Run in Local Browser
+```
+npm test
+
+```
+
+#### Screenshot
+![alt text](<Screenshot at Dec 27 11-34-51.png>)
--- test/api.test.ts
+++ test/api.test.ts
@@ -16,56 +16,20 @@ describe('Reqres API Tests', () => {
     describe('GET Single User', () => {
         it('should get a single user successfully', async () => {
             const response = await axios.get<SingleUserResponse>(`${BASE_URL}/users/2`);
-            
+            // Check status code
             expect(response.status).to.equal(200);
-            
             // Check data structure
             expect(response.data.data).to.have.property('id');
             expect(response.data.data).to.have.property('email');
             expect(response.data.data).to.have.property('first_name');
             expect(response.data.data).to.have.property('last_name');
             expect(response.data.data).to.have.property('avatar');
-
-            // Check specific values for user data
-            // expect(response.data.data.id).to.equal(2);
-            // expect(response.data.data.email).to.equal('janet.weaver@reqres.in');
-            // expect(response.data.data.first_name).to.equal('Janet');
-            // expect(response.data.data.last_name).to.equal('Weaver');
-            // expect(response.data.data.avatar).to.equal('https://reqres.in/img/faces/2-image.jpg');
-
             // Check support object structure
             expect(response.data.support).to.have.property('url');
             expect(response.data.support).to.have.property('text');
 
-            // Check specific values for support object
-            // expect(response.data.support.url).to.equal('https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral');
-            // expect(response.data.support.text).to.equal('Tired of writing endless social media content? Let Content Caddy generate it for you.');
-
-            // Alternative way to check the entire response at once
-            expect(response.data).to.deep.equal({
-                data: {
-                    id: 2,
-                    email: 'janet.weaver@reqres.in',
-                    first_name: 'Janet',
-                    last_name: 'Weaver',
-                    avatar: 'https://reqres.in/img/faces/2-image.jpg'
-                },
-                support: {
-                    url: 'https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral',
-                    text: 'Tired of writing endless social media content? Let Content Caddy generate it for you.'
-                }
-            }); 
         });
 
-        it('should return 404 for non-existent user', async () => {
-            try {
-                await axios.get(`${BASE_URL}/users/23`);
-                throw new Error('Expected 404 error but got success');
-            } catch (error) {
-                const axiosError = error as any;
-                expect(axiosError.response?.status).to.equal(404);
-            }
-        });
     });
 
     describe('POST Register', () => {
